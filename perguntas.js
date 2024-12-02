61aa20ba89a3d9289d5ffa2776f9fd4ee3f0a7ec
Rafael biano <164901522+rbiano@users.noreply.github.com>
2024-12-02 18:15:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Futebol',
+    'Qual Maior Clube do Brasil',
+    'Corinthians é claro'
+)
+criaCartao(
+    'Torcida',
+    'Qual a Maior Torcida do Brasil ',
+    'O Bando de Loucos'
+)
+criaCartao(
+    'Estadio',
+    'Qual o estadio mais lindo ',
+    'Neo Quimica Arena'
+)
+criaCartao(
+    'Mundial',
+    'Palmeiras tem Mundial',
+    'Claro que Não,51 é Pinga'
+)
